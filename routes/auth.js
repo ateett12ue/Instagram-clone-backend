844c856a08c050854d01524495196b51a16f9125
ateet <ateet@propelld.com>
2020-08-01 21:58:06 +0530

    signin signup concept

---
+++ routes/auth.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+const brcypt = require("bcryptjs")
+router.get('/', (req,res)=>{
+    res.send("hello")
+});
+
+router.post('/signup', (req,res)=>{
+    const {name, email, password} = req.body;
+    if(!email || !password || !name){
+        return res.status(422).json({error: "please add all the fields"})    
+    }
+    User.findOne({email:email}).then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error: "User already exits with this email"})
+        }
+        brcypt.hash(password, 12).then(hashedPassword=>{
+            const user = new User({
+                email,
+                password: hashedPassword,
+                name
+            })
+            user.save().then(user=>{
+                res.json({message: "saved successfully"})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        })
+        
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+router.post('/signin', (req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        req.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email}).then(savedUser=>{
+        if(!savedUser){
+           return res.status(422).json({error: "Invalid Email or Password"}) 
+        }
+        brcypt.compare(password, savedUser.password).then(doMatch=>{
+            if(doMatch){
+                return res.json({message:"Successfully sign in"})
+            }
+            else{
+                return res.status(422).json({error: "Invalid Email or Password"}) 
+            }
+        })
+    }).catch(err=>{
+        console.log(err);
+    })
+})
+
+
+
+module.exports = router;
