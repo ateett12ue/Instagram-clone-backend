eca97920459ba82271ff0332c91faa062fb62a1c
ateet <ateet@propelld.com>
2020-08-10 23:33:04 +0530

    added like and unlike post in server side apis

--- routes/post.js
+++ routes/post.js
@@ -1,44 +1,79 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const requireLogin = require("../middleware/requireLogin")
+const mongoose = require("mongoose");
+const requireLogin = require("../middleware/requireLogin");
 const Post = mongoose.model("Post");
 
-router.get('/allpost',(req,res)=>{
-    Post.find().populate("postedBy","_id name").then(
-        post=>{
-            res.json({post})
-        }
-    ).catch(err =>{
-        console.log(err)
+router.get("/allpost", requireLogin, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .then((post) => {
+      res.json({ post });
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.post('/createpost',requireLogin,(req,res)=>{
-    const {title, body, pic} = req.body;
-    if(!title || !body ||!pic){
-        res.status(422).json({error:"Please add all the fields"})
+router.post("/createpost", requireLogin, (req, res) => {
+  const { title, body, pic } = req.body;
+  if (!title || !body || !pic) {
+    res.status(422).json({ error: "Please add all the fields" });
+  }
+  req.user.password = undefined;
+  const post = new Post({
+    title,
+    body,
+    photo: pic,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.json({ post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/mypost", requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("PostedBy", "_id name")
+    .then((mypost) => {
+      res.json({ mypost });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.put("/like", requireLogin, (req,res)=>{
+  Post.findByIdAndUpdate(req.body.postId,{
+    $push:{likes:req.user._id}
+  },{
+    new:true
+  }).exec((err, result)=>{
+    if(err){
+      return res.status(422).json({error:err})
     }
-    req.user.password = undefined;
-    const post = new Post({
-        title,
-        body,
-        photo:pic,
-        postedBy: req.user
-    })
-    post.save().then(result=>{
-        res.json({post:result})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
+    else{
+      res.json(result)
+    }
+  }) 
 })
 
-router.get('/mypost',requireLogin, (req,res)=>{
-    Post.find({postedBy:req.user._id}).populate("PostedBy","_id name").then(mypost => {
-        res.json({mypost})
-    })
-    .catch(err=>console.log(err))
+router.put("/unlike", requireLogin, (req,res)=>{
+  Post.findByIdAndUpdate(req.body.postId,{
+    $pull:{likes:req.user._id}
+  },{
+    new:true
+  }).exec((err, result)=>{
+    if(err){
+      return res.status(422).json({error:err})
+    }
+    else{
+      res.json(result)
+    }
+  }) 
 })
 
-module.exports = router
+module.exports = router;
