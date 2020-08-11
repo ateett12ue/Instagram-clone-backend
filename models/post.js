f2c3ef062d7758344c7e285c0be07e8eec16134d
ateet <ateet@propelld.com>
2020-08-11 22:39:26 +0530

    added comment logic and delete post logic

--- models/post.js
+++ models/post.js
@@ -21,7 +21,7 @@ const postSchema = new mongoose.Schema({
   ],
   comments: [
     {
-      type: String,
+      text: String,
       postedBy: { type: ObjectId, ref: "User" },
     },
   ],
--- routes/post.js
+++ routes/post.js
@@ -7,6 +7,7 @@ const Post = mongoose.model("Post");
 router.get("/allpost", requireLogin, (req, res) => {
   Post.find()
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
     .then((post) => {
       res.json({ post });
     })
@@ -46,55 +47,87 @@ router.get("/mypost", requireLogin, (req, res) => {
     .catch((err) => console.log(err));
 });
 
-router.put("/like", requireLogin, (req,res)=>{
-  Post.findByIdAndUpdate(req.body.postId,{
-    $push:{likes:req.user._id}
-  },{
-    new:true
-  }).exec((err, result)=>{
-    if(err){
-      return res.status(422).json({error:err})
+router.put("/like", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { likes: req.user._id },
+    },
+    {
+      new: true,
     }
-    else{
-      res.json(result)
-    }
-  }) 
-})
+  ) .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
+});
 
-router.put("/unlike", requireLogin, (req,res)=>{
-  Post.findByIdAndUpdate(req.body.postId,{
-    $pull:{likes:req.user._id}
-  },{
-    new:true
-  }).exec((err, result)=>{
-    if(err){
-      return res.status(422).json({error:err})
+router.put("/unlike", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $pull: { likes: req.user._id },
+    },
+    {
+      new: true,
     }
-    else{
-      res.json(result)
-    }
-  }) 
-})
+  ) .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
+});
 
-router.put("/comment", requireLogin, (req,res)=>{
+router.put("/comment", requireLogin, (req, res) => {
   const comment = {
     text: req.body.text,
-    postedBy: req.user._id
-  }
-  Post.findByIdAndUpdate(req.body.postId,{
-    $pull:{comments:comment}
-  },{
-    new:true
-  }).
-  populate("comments.postedBy","_id name")
-  .exec((err, result)=>{
-    if(err){
-      return res.status(422).json({error:err})
+    postedBy: req.user._id,
+  };
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { comments: comment },
+    },
+    {
+      new: true,
     }
-    else{
-      res.json(result)
-    }
-  }) 
+  )
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+router.delete('/deletepost/:postId',requireLogin, (req,res)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy","_id")
+    .exec((err, post)=>{
+      if(err || !post){
+        return res.status(422).json({error:err})
+      }
+      if(String(post.postedBy._id) == String(req.user._id)){
+        post.remove().then(result=>{
+          res.json(result)
+        }).catch(err=>{
+          console.log(err)
+        })
+      }
+    })
 })
 
 module.exports = router;
